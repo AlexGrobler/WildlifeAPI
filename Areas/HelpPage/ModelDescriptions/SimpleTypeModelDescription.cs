b1990c8d463fc827392b43be6b82e76ac931da46
space-commits <57108281+space-commits@users.noreply.github.com>
2024-04-23 23:50:24 +0100

    Add project files.

---
+++ Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WildlifeAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Controllers/WildlifeAPIController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WildlifeAPI.Models;
+
+namespace WildlifeAPI.Controllers
+{
+    public class WildlifeAPIController : ApiController
+    {
+        [Route("api/WildlifeSightings/{speciesId}")]
+        public Sighting Get(string speciesId)
+        {
+            //ensures that the instance of this db connection is disposed of in memory...
+            //immediately rather than waiting for GC.
+            using (wildlife_sightings_DBEntities db = new wildlife_sightings_DBEntities())
+            {
+                return db.Sightings.Find(speciesId);
+            }
+        }
+    }
+}
