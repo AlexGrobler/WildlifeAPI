2510e0e31c90b137ff76e6ca0157a16d3b3f6143
space-commits <57108281+space-commits@users.noreply.github.com>
2024-04-30 17:26:57 +0100

    made endpoints async

--- Controllers/WildlifeAPIController.cs
+++ Controllers/WildlifeAPIController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 using WildlifeAPI.Models;
 using static System.Net.WebRequestMethods;
@@ -13,11 +15,11 @@ namespace WildlifeAPI.Controllers
     {
         [HttpGet]
         [Route("api/WildlifeSightings/BySpecies/{speciesId}")]
-        public IHttpActionResult GetSightingsBySpecies(int speciesId)
+        public async Task<IHttpActionResult> GetSightingsBySpecies(int speciesId)
         {
             using (wildlife_sightings_DBEntities db = new wildlife_sightings_DBEntities())
             {
-                var sightings = db.Sightings.Where(s => s.SpeciesID == speciesId).ToList();
+                var sightings = await db.Sightings.Where(s => s.SpeciesID == speciesId).ToListAsync();
                 if (sightings != null)
                 {
                     return Ok(sightings);  //follows RESTFul API principles to return feedback to the client
@@ -28,21 +30,21 @@ namespace WildlifeAPI.Controllers
 
         [HttpGet]
         [Route("api/WildlifeSightings/All")]
-        public IHttpActionResult GetAllSightings()
+        public async Task<IHttpActionResult> GetAllSightings()
         {
             using (wildlife_sightings_DBEntities db = new wildlife_sightings_DBEntities())
             {
-                return Ok(db.Sightings.ToList());  //follows RESTFul API principles to return feedback to the client
+                return Ok(await db.Sightings.ToListAsync());  //follows RESTFul API principles to return feedback to the client
             }
         }
 
         [HttpGet]
         [Route("api/WildlifeSightings/BySighting/{id}")]
-        public IHttpActionResult Get(int id)
+        public async Task<IHttpActionResult> Get(int id)
         {
             using (wildlife_sightings_DBEntities db = new wildlife_sightings_DBEntities())
             {
-                var sighting = db.Sightings.Find(id);
+                var sighting = await db.Sightings.FindAsync(id);
                 if (sighting != null)
                 {
                     return Ok(sighting);  //follows RESTFul API principles to return feedback to the client
@@ -53,14 +55,14 @@ namespace WildlifeAPI.Controllers
 
         [HttpPost]
         [Route("api/WildlifeSightings/CreateSighting")]
-        public IHttpActionResult CreateSighting(Sighting sighting)
+        public async Task<IHttpActionResult> CreateSighting(Sighting sighting)
         {
             using (var db = new wildlife_sightings_DBEntities())
             {
                 if (ModelState.IsValid)
                 {
                     db.Sightings.Add(sighting);
-                    db.SaveChanges();
+                    await db.SaveChangesAsync();
                     return CreatedAtRoute("Default", new { id = sighting.ID }, sighting); //follows RESTFul API principles to return feedback to the client
                 }
                 return BadRequest(ModelState);
@@ -69,20 +71,20 @@ namespace WildlifeAPI.Controllers
 
         [HttpPost]
         [Route("api/WildlifeSightings/UpdateSighting")]
-        public IHttpActionResult UpdateSighting(Sighting sighting)
+        public async Task<IHttpActionResult> UpdateSighting(Sighting sighting)
         {
             using (var db = new wildlife_sightings_DBEntities()) 
             {
                 if (ModelState.IsValid)
                 {
-                    var dbSighting = db.Sightings.FirstOrDefault(s => s.ID == sighting.ID);
+                    var dbSighting = await db.Sightings.FirstOrDefaultAsync(s => s.ID == sighting.ID);
                     if (dbSighting == null)
                     {
                         return NotFound();
                     }
 
                     db.Entry(dbSighting).CurrentValues.SetValues(sighting);
-                    db.SaveChanges();
+                    await db.SaveChangesAsync();
                     return Ok(dbSighting); //follows RESTFul API principles to return feedback to the client
                 }
                 return BadRequest(ModelState);
@@ -91,20 +93,20 @@ namespace WildlifeAPI.Controllers
 
         [HttpDelete]
         [Route("api/WildlifeSightings/DeleteSighting/{id}")]
-        public IHttpActionResult DeleteSighting(int id)
+        public async Task<IHttpActionResult> DeleteSighting(int id)
         {
             using (var db = new wildlife_sightings_DBEntities())
             {
                 try
                 {
-                    var dbSighting = db.Sightings.Find(id);
+                    var dbSighting = await db.Sightings.FindAsync(id);
                     if (dbSighting == null)
                     {
                         return NotFound();
                     }
 
                     db.Sightings.Remove(dbSighting);
-                    db.SaveChanges();
+                    await db.SaveChangesAsync();
                     return Ok(dbSighting);
                 }
                 catch (Exception ex)
